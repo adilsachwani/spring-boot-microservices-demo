727e742b083f6521a497f78238229fc7409d3051
adilsachwani <adilsachwani@gmail.com>
2022-11-03 12:15:06 +0500

    register order service as eureka client

--- order-service/src/main/java/com/adilsachwani/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/adilsachwani/orderservice/OrderServiceApplication.java
@@ -2,9 +2,11 @@ package com.adilsachwani.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderServiceApplication {
 
